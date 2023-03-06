674a9996c009c42d82d0d7e377b5723bc073dd59
Nicolas Coulange <coulange.n@gmail.com>
2023-03-06 18:12:34 +0100

    trying multithread scraping

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -39,41 +39,11 @@ export class AppController {
 
     const departements = await this.listDepartements(driver);
 
-    for (const departement of departements) {
-      await driver.findElement(By.name('departement')).click();
-      await driver
-        .findElement(By.css(`option[value='${departement}']`))
-        .click();
-      const communes = await this.listCommunes(driver);
-      for (const commune of communes) {
-        if (
-          (await this.redis.hexists('yuka.communes-raw-table', commune)) === 1
-        ) {
-          continue;
-        }
-
-        try {
-          await driver.findElement(By.name('communeDepartement')).click();
-          await driver
-            .findElement(By.css(`option[value='${commune}']`))
-            .click();
-          await driver.findElement(By.name('btnRechercher')).click();
-          const tables = await driver.findElements(By.css('table'));
-          await this.redis.hset(
-            'yuka.communes-raw-table',
-            commune,
-            JSON.stringify({
-              commune,
-              generalInformations: await tables[0].getAttribute('innerHTML'),
-              results: await tables[2].getAttribute('innerHTML'),
-            }),
-          );
-          console.log(`Fetched ${commune}`);
-        } catch (e) {
-          console.log('damn, an error', { commune, e });
-        }
-      }
-    }
+    await Promise.all(
+      departements.map((departement: string) =>
+        this.scrapeDepartement(departement),
+      ),
+    );
 
     // await driver.findElement(By.name('btnRechercher')).click();
     await driver.quit();
